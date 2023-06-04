0f5c53f033f68723443847532426c0109bca77d3
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-06-04 20:17:45 +0600

    volunteer network site is building up

---
+++ src/components/Admin/EventAdd/EventAdd.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EventAdd = () => {
+    return (
+        <div>
+            <h1>Event Add</h1>
+        </div>
+    );
+};
+
+export default EventAdd;
---
+++ src/components/Admin/RegisterList/RegisterList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RegisterList = () => {
+    return (
+        <div>
+            <h1>Register List</h1>
+        </div>
+    );
+};
+
+export default RegisterList;
