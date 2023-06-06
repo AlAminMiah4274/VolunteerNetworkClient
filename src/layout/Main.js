121c2dbb5eeed8e3abff30ade626d0b4a913d92c
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-06-06 10:25:11 +0600

    added react-bootstrap and modified main and routers components

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from '../components/Shared/Header/Header';
+import Footer from '../components/Shared/Footer/Footer';
 
 const Main = () => {
     return (
         <div>
-            <h1>Main</h1>
+            <Header></Header>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
